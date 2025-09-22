2dfc48cf468e4041ffa4a0497c73eba86a863a50
Pauline <some@mail>
2025-09-22 18:55:27 +0300

    done5

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Random ran = new Random();
         String[] names = {"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"};
         for(int i = 1; i<=10; i++){
-            MyThread mt = new MyThread(names[i], ran.nextInt(1, 5), ran.nextInt(5, 11));
+            MyThread mt = new MyThread(names[i-1], ran.nextInt(1, 5), ran.nextInt(5, 11));
             System.out.println("\nПоток \""+mt.name+"\" №"+i);
             mt.run();
         }
